e27ed84d4d63d76c25f95b26d6f179a2427477af
estefany <es.bozo1994@gmail.com>
2021-06-19 13:14:06 -0400

    incorporacion js

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+
+$("a").click(function(event){
+if (this.hash !==""){
+event.preventdefault();
+var gato = this.hash;
+$("html,body").animate({
+scrolltop: $(gato).offset().top},2000,function(){
+window.location.hash=gato;
+
+
+
+});
+}
+});
+
