7c61f55700e0f5a016906f076cb9ae8472cf82b8
estefany <es.bozo1994@gmail.com>
2021-06-20 19:28:19 -0400

    proyecto terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,45 @@ window.location.hash=gato;
 }
 });
 
+
+
+//tooltip seccion tarjetas 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
+
+
+  
+  
+  
+  
+  //modal img 1 seccion tarjetas 
+  $(document).ready(function () {
+    $(".foto-1").click(function () {
+        $("#foto-1").modal("show");
+    });
+});
+   //modal img 2 seccion tarjetas 
+
+ $(document).ready(function () {
+    $(".foto-2").click(function () {
+        $("#foto-2").modal("show");
+    });
+});
+
+   //modal img 3 seccion tarjetas 
+
+   $(document).ready(function () {
+    $(".foto-3").click(function () {
+        $("#foto-3").modal("show");
+    });
+});
+ 
+
+  //modal img 4 seccion tarjetas 
+
+  $(document).ready(function () {
+    $(".foto-4").click(function () {
+        $("#foto-4").modal("show");
+    });
+});
